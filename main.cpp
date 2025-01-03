14883fc8f752f67f56594be577621d8a073ba4bb
RobertFigenscher <robert.figenscher@upr.edu>
2025-01-03 18:56:43 +0000

    cambios

--- main.cpp
+++ main.cpp
@@ -12,9 +12,9 @@ int main()
     ifstream inputFile;
     inputFile.open("words.txt");
     string line;
+    srand(time(0));
     while (getline(inputFile, line)) words.push_back(line);
-
-    cout << words[0] << endl;
+     cout << words[rand() % words.size()] << endl;
 
     return 0;
 } 
