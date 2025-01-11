69a4c0ca00369923002c05a88160e083a4a7e918
RobertFigenscher <robert.figenscher@upr.edu>
2025-01-11 20:54:42 +0000

    input y validation

--- main.cpp
+++ main.cpp
@@ -13,10 +13,20 @@ int main()
     ifstream inputFile;
     inputFile.open("words.txt");
     string line;
+    string userWord;
     srand(time(0));
     while (getline(inputFile, line)) words.push_back(line);
     wordOfGame = words[rand() % words.size()];
     cout << wordOfGame << endl;
+
+    cin >> userWord; 
+    while (userWord.length() != 5) 
+    {
+        cout << "Invalid input, try again." << endl;
+        cin >> userWord; 
+    }
+
+
 
     return 0;
 } 
