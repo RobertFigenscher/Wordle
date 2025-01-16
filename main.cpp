20814a4ca66e1f4077e5356c7b6122796e478577
RobertFigenscher <robert.figenscher@upr.edu>
2025-01-16 15:30:06 +0000

    cambios

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main()
     inputFile.open("words.txt");
     string line;
     string userWord;
-    bool flag1;
+    bool flag1 = false, flag2 = false, flag3 = false, flag4 = false, flag5 = false;
     srand(time(0));
     while (getline(inputFile, line)) words.push_back(line);
     wordOfGame = words[rand() % words.size()];
@@ -47,13 +47,60 @@ int main()
         for (int i = 0; i <= 5; i++)
         {
             if (userWord[1] == wordOfGame[i])
-               flag1 = true;
+               flag2 = true;
         }
-        if (flag1)
+        if (flag2)
+            cout << "letter is in word" << endl;
+        else 
+            cout << "letter is not in word" << endl;
+
+    }
+      if (userWord[2] == wordOfGame[2])
+        cout << "Same third letter!" << endl;
+    else 
+    {
+        for (int i = 0; i <= 5; i++)
+        {
+            if (userWord[2] == wordOfGame[i])
+               flag3 = true;
+        }
+        if (flag3)
             cout << "letter is in word" << endl;
         else 
             cout << "letter is not in word" << endl;
     }
+
+          if (userWord[3] == wordOfGame[3])
+        cout << "Same fourth letter!" << endl;
+    else 
+    {
+        for (int i = 0; i <= 5; i++)
+        {
+            if (userWord[3] == wordOfGame[i])
+               flag4 = true;
+        }
+        if (flag4)
+            cout << "letter is in word" << endl;
+        else 
+            cout << "letter is not in word" << endl;
+    }
+
+          if (userWord[4] == wordOfGame[4])
+        cout << "Same fifth letter!" << endl;
+    else 
+    {
+        for (int i = 0; i <= 5; i++)
+        {
+            if (userWord[4] == wordOfGame[i])
+               flag5 = true;
+        }
+        if (flag5)
+            cout << "letter is in word" << endl;
+        else 
+            cout << "letter is not in word" << endl;
+    }
+
+
 
     return 0;
 } 
