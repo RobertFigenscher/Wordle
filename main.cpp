aa855ca2041439acecf6a8fa276d1647a00b46d8
RobertFigenscher <robert.figenscher@upr.edu>
2025-01-17 18:32:25 +0000

    cambios

--- main.cpp
+++ main.cpp
@@ -17,87 +17,92 @@ int main()
     bool flag1 = false, flag2 = false, flag3 = false, flag4 = false, flag5 = false;
     srand(time(0));
     while (getline(inputFile, line)) words.push_back(line);
-    wordOfGame = words[rand() % words.size()];
-    cout << wordOfGame << endl;
-
-    cin >> userWord; 
-    while (userWord.length() != 5) 
+    wordOfGame = words[rand() % words.size()]; 
+    cout << "Intente encontrar la palabra de cinco letras en menos de 5 intentos" << endl;
+    for(int tries = 1; tries<= 5; tries++)
     {
-        cout << "Invalid input, try again." << endl;
+        cout << "intento: " << tries << endl;
+        cout << wordOfGame << endl;
         cin >> userWord; 
-    }
-    if (userWord[0] == wordOfGame[0])
-        cout << "Same first letter!" << endl;
-    else 
-    {
-        for (int i = 0; i <= 5; i++)
+        while (userWord.length() != 5) 
         {
-            if (userWord[0] == wordOfGame[i])
-               flag1 = true;
+            cout << "Invalid input, try again." << endl;
+            cin >> userWord; 
         }
-        if (flag1)
-            cout << "letter is in word" << endl;
+        if (userWord[0] == wordOfGame[0])
+            cout << "Same first letter!" << endl;
         else 
-            cout << "letter is not in word" << endl;
-    } 
-    if (userWord[1] == wordOfGame[1])
-        cout << "Same second letter!" << endl;
-    else 
-    {
-        for (int i = 0; i <= 5; i++)
         {
-            if (userWord[1] == wordOfGame[i])
-               flag2 = true;
-        }
+            for (int i = 0; i <= 5; i++)
+            {
+                if (userWord[0] == wordOfGame[i])
+                    flag1 = true;
+            }
+            if (flag1)
+                cout << "letter is in word" << endl;
+            else 
+                cout << "letter is not in word" << endl;
+        } 
+
+        if (userWord[1] == wordOfGame[1])
+        cout << "Same second letter!" << endl;
+        else 
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                if (userWord[1] == wordOfGame[i])
+                    flag2 = true;
+            }
         if (flag2)
             cout << "letter is in word" << endl;
         else 
             cout << "letter is not in word" << endl;
-
-    }
-      if (userWord[2] == wordOfGame[2])
-        cout << "Same third letter!" << endl;
-    else 
-    {
-        for (int i = 0; i <= 5; i++)
-        {
-            if (userWord[2] == wordOfGame[i])
-               flag3 = true;
+        
         }
-        if (flag3)
-            cout << "letter is in word" << endl;
+        if (userWord[2] == wordOfGame[2])
+            cout << "Same third letter!" << endl;
         else 
-            cout << "letter is not in word" << endl;
-    }
-
-          if (userWord[3] == wordOfGame[3])
-        cout << "Same fourth letter!" << endl;
-    else 
-    {
-        for (int i = 0; i <= 5; i++)
         {
-            if (userWord[3] == wordOfGame[i])
-               flag4 = true;
+            for (int i = 0; i <= 5; i++)
+            {
+                if (userWord[2] == wordOfGame[i])
+                    flag3 = true;
+            }
+            if (flag3)
+                cout << "letter is in word" << endl;
+            else 
+                cout << "letter is not in word" << endl;
         }
-        if (flag4)
-            cout << "letter is in word" << endl;
+
+        if (userWord[3] == wordOfGame[3])
+            cout << "Same fourth letter!" << endl;
         else 
-            cout << "letter is not in word" << endl;
-    }
-
-          if (userWord[4] == wordOfGame[4])
-        cout << "Same fifth letter!" << endl;
-    else 
-    {
-        for (int i = 0; i <= 5; i++)
         {
-            if (userWord[4] == wordOfGame[i])
-               flag5 = true;
+            for (int i = 0; i <= 5; i++)
+            {
+                if (userWord[3] == wordOfGame[i])
+                    flag4 = true;
+            }
+            if (flag4)
+                cout << "letter is in word" << endl;
+            else 
+                cout << "letter is not in word" << endl;
         }
+
+        if (userWord[4] == wordOfGame[4])
+            cout << "Same fifth letter!" << endl;
+        else 
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                if (userWord[4] == wordOfGame[i])
+                    flag5 = true;
+            }
         if (flag5)
             cout << "letter is in word" << endl;
         else 
             cout << "letter is not in word" << endl;
+        }
     }
 
 
